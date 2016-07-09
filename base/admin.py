7c6f747cd904a592fabe53e0824929eddcfa5504
satyamsammi <satyamsammi@gmail.com>
2016-07-09 17:50:24 +0530

    first_change

---
+++ base/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from base.models import *
+# Register your models here.
+
+admin.site.register(HomeImages)
+
+admin.site.register(Product)
+
+admin.site.register(Review)
+
+admin.site.register(ReviewImages)
+
